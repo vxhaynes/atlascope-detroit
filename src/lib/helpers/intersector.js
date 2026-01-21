7f7904f4d041b7ec2dfa23b5523fe86f672561cb
Ian Spangler <39773984+itspangler@users.noreply.github.com>
2026-01-21 13:07:19 -0500

    fix polygon interpretation in intersector.js

--- src/lib/helpers/intersector.js
+++ src/lib/helpers/intersector.js
@@ -1,12 +1,12 @@
 import intersect from "@turf/intersect";
 import bboxPolygon from "@turf/bbox-polygon";
 import area from "@turf/area";
-import { featureCollection, polygon, multiPolygon, point} from "@turf/turf";
+import { featureCollection, multiPolygon, point} from "@turf/turf";
 import booleanPointInPolygon from '@turf/boolean-point-in-polygon';
 
 export const intersector = (footprint, extent) => {
     const extentPolygon = bboxPolygon(extent);
-    const intersection = intersect(featureCollection([polygon(footprint.coordinates), extentPolygon]));
+    const intersection = intersect(featureCollection([multiPolygon(footprint.coordinates), extentPolygon]));
     if(intersection == null) {
         return 0;
     } else {
@@ -16,6 +16,6 @@ export const intersector = (footprint, extent) => {
 
 export const insideChecker = (ll, poly) => {
     const pt = point(ll);
-    const pol = polygon(poly[0].coordinates);
-    return booleanPointInPolygon(pt, pol);
+    const polygon = multiPolygon(poly.coordinates);
+    return booleanPointInPolygon(pt, polygon);
   };
